9d11f85e6d1dfb1f65acbde24e5a3948e50b7c1f
Lucas Cassiano Pontes <lucasuqb@gmail.com>
2024-10-14 15:50:21 -0300

    tela de cadastro

---
+++ mudancas/sistema-youtan-front1/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+
+function App() {
+
+  return (
+    <SignUp/>
+  )
+}
+
+export default App
---
+++ mudancas/sistema-youtan-front1/frontend/src/routes/SignUp.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
+
+const Cadastro = () => {
+    return (
+        <SignUp/>
+    );
+};
+
+export default Cadastro;
