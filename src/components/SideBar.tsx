4d0eaf575048b9f0752cf1b14c05fd435bbec95b
yrnThiago <thiagodias_123@hotmail.com>
2024-11-09 19:18:31 -0300

    feat: add new components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -17,38 +17,114 @@ const SideBarLayout = () => {
     localStorage.removeItem("is-auth");
     window.location.href = "/login"
   }
+
+  
+  type UserRole = "Admin" | "Líder" | "Líder e Liderado" | "Liderado" | null;
+  const userRole = localStorage.getItem("is-auth") as UserRole;
+
+  type RoleLinks = {
+    [key: string]: { endpoint: string; component: React.ComponentType<{ className?: string }>; name: string }[];
+    Admin: { endpoint: string; component: React.ComponentType<{ className?: string }>; name: string }[];
+  };
+  const roleBtns: RoleLinks = {
+    "Admin": [
+      {
+        "endpoint": "/dashboard",
+        "component": LayoutDashboard,
+        "name": "Dashboard"
+      },
+      {
+        "endpoint": "/usuarios",
+        "component": User,
+        "name": "Usuários"
+      },
+      {
+        "endpoint": "/times",
+        "component": UsersRound,
+        "name": "Times"
+      },
+      {
+        "endpoint": "/formularios",
+        "component": Text,
+        "name": "Formulários"
+      },
+    ],
+    "Líder": [
+      {
+        "endpoint": "/dashboard",
+        "component": LayoutDashboard,
+        "name": "Dashboard"
+      },
+      {
+        "endpoint": "/times",
+        "component": UsersRound,
+        "name": "Meu time"
+      },
+      {
+        "endpoint": "/formularios/responder",
+        "component": Text,
+        "name": "Formulários"
+      },
+    ],
+    "Líder e Liderado": [
+      {
+        "endpoint": "/dashboard",
+        "component": LayoutDashboard,
+        "name": "Dashboard"
+      },
+      {
+        "endpoint": "/times",
+        "component": UsersRound,
+        "name": "Meu time"
+      },
+      {
+        "endpoint": "/formularios/responder",
+        "component": Text,
+        "name": "Formulários"
+      },
+    ],
+    "Liderado": [
+      {
+        "endpoint": "/dashboard",
+        "component": LayoutDashboard,
+        "name": "Dashboard"
+      },
+      {
+        "endpoint": "/formularios/responder",
+        "component": Text,
+        "name": "Formulários"
+      },
+    ]
+  }
   return (
     <aside className="w-64 bg-white shadow-md">
           <div className="p-4">
               <h1 className="text-2xl font-bold text-blue-600">YOUTAN</h1>
           </div>
           <nav className="mt-8">
-            <a href="/dashboard" className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-200">
-                <LayoutDashboard className="mr-3 h-5 w-5" />
-                  Dashboard
+
+            {userRole? roleBtns[userRole].map((btn, idx) => {
+              const ComponentIcon = btn.component;
+              
+              return (
+                <a href={btn.endpoint} className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-200" key={idx}>
+                  <ComponentIcon className="mr-3 h-5 w-5" />
+                  {btn.name}
                 </a>
-              <a href="/usuarios" className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-200">
-                <User className="mr-3 h-5 w-5" />
-                  Usuários
-                </a>
-              <a href="/times" className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-200">
-                <UsersRound className="mr-3 h-5 w-5" />
-                  Times
-              </a>
-              <a href="/formularios" className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-200">
-                <Text className="mr-3 h-5 w-5" />
-                  Formulários
-              </a>
+              );
+            }) : window.location.href = "/login"}
+
           </nav>
+
           <div className="absolute bottom-0 w-64 p-4">
-              <a href="#" className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-200">
+            <a href="#" className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-200">
               <Settings className="mr-3 h-5 w-5" />
-              Configurações
-              </a>
-              <a href="#" onClick={() => {logout()}} className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-200">
+                Configurações
+            </a>
+            <a href="#" onClick={logout} className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-200">
               <LogOut className="mr-3 h-5 w-5" />
-              Sair
-              </a>
+                Sair
+            </a>
           </div>
       </aside>
   )
